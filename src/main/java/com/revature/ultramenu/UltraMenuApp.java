868cc665d9ec7b3a1bd5b4b8f0f806437745c4d4
Daut baterbiev <baterbievd@gmail.com>
2022-08-29 23:02:39 -0400

    connectfactory created

--- src/main/java/com/revature/ultramenu/UltraMenuApp.java
+++ src/main/java/com/revature/ultramenu/UltraMenuApp.java
@@ -2,7 +2,8 @@ package com.revature.ultramenu;
 
 public class UltraMenuApp {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+
+        new AppContext().startApp();
     }
 }
